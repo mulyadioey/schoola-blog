8bd548ca1ee7b65c8e9f62b86d97fd8678de2adf
Mulyadi Oey <oey.mulyadi@gmail.com>
2014-09-09 20:28:28 -0700

    Post page.

---
+++ wp-content/themes/schoola/docs/post.php
@@ -0,0 +1,139 @@
+<?php include('includes/tmpl.php'); ?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="description" content="[description]" />
+    <meta name="viewport" content="width=1000">
+
+    <title>Schoola Blog</title>
+
+    <link rel="shortcut icon" href="/images/favicon.ico" />
+
+    <link type="text/css" rel="stylesheet" href="../assets/css/vendor/bootstrap/bootstrap.min.css" />
+    <link type="text/css" rel="stylesheet" href="../assets/css/vendor/font-awesome/font-awesome.min.css" />
+    <link type="text/css" rel="stylesheet" href="../assets/css/schoola-blog.css" />
+</head>
+<body class="schoola-blog">
+<div class="s-container">
+
+    <div class="clearfix">
+
+        <div class="blog-content">
+
+            <div class="module big">
+                <img src="../assets/img/tmp/module-1.jpg" />
+                <?php module_ribbon(array('category' => 'art', 'with-comment' => true)); ?>
+            </div>
+            <div class="block-head">
+                <h1>Why art actually really matters</h1>
+            </div>
+
+            <div class="post-meta">
+                <p class="post-author">Heather Bronson</p>
+                <p class="post-date">2 days ago</p>
+            </div>
+
+            <div class="post-body">
+                <p>Art can be one of the most important parts of a young student&rsquo;s life. It teaches kids that it&rsquo;s okay to be creative and take risks at school—not just in the art room, but in math, languages, and every other subject they study. The educational benefits of an art program are well known, but it&rsquo;s still not covered by most public school budgets. The parents at Yick Wo Elementary make sure that their school’s students have art class anyway, and Schoola is forming a community to help them do it.</p>
+                <div class="image-l">
+                    <img src="../assets/img/tmp/random-1.jpg" />
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam blandit gravida pharetra. Nullam nisl justo, malesuada nec arcu eu, sagittis placerat leo. Integer tempus metus orci, ac condimentum urna pharetra eget. Donec a feugiat libero, eu ultrices nibh. Mauris nec nunc vel nunc congue accumsan. Quisque mattis dolor non dolor interdum ultrices. Mauris faucibus urna et velit scelerisque, vitae condimentum massa tristique. In arcu metus, rutrum eu eleifend ac, eleifend in metus. Fusce gravida, risus non posuere ultrices, justo lectus dictum sapien, id scelerisque justo nulla nec justo.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam blandit gravida pharetra. Nullam nisl justo, malesuada nec arcu eu, sagittis placerat leo. Integer tempus metus orci, ac condimentum urna pharetra eget. Donec a feugiat libero, eu ultrices nibh. Mauris nec nunc vel nunc congue accumsan. Quisque mattis dolor non dolor interdum ultrices. Mauris faucibus urna et velit scelerisque, vitae condimentum massa tristique. In arcu metus, rutrum eu eleifend ac, eleifend in metus. Fusce gravida, risus non posuere ultrices, justo lectus dictum sapien, id scelerisque justo nulla nec justo.</p>
+                </div>
+                <div class="image-r">
+                    <img src="../assets/img/tmp/random-2.jpg" />
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam blandit gravida pharetra. Nullam nisl justo, malesuada nec arcu eu, sagittis placerat leo. Integer tempus metus orci, ac condimentum urna pharetra eget. Donec a feugiat libero, eu ultrices nibh. Mauris nec nunc vel nunc congue accumsan. Quisque mattis dolor non dolor interdum ultrices. Mauris faucibus urna et velit scelerisque, vitae condimentum massa tristique. In arcu metus, rutrum eu eleifend ac, eleifend in metus. Fusce gravida, risus non posuere ultrices, justo lectus dictum sapien, id scelerisque justo nulla nec justo.</p>
+                </div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam blandit gravida pharetra. Nullam nisl justo, malesuada nec arcu eu, sagittis placerat leo. Integer tempus metus orci, ac condimentum urna pharetra eget. Donec a feugiat libero, eu ultrices nibh. Mauris nec nunc vel nunc congue accumsan. Quisque mattis dolor non dolor interdum ultrices. Mauris faucibus urna et velit scelerisque, vitae condimentum massa tristique. In arcu metus, rutrum eu eleifend ac, eleifend in metus. Fusce gravida, risus non posuere ultrices, justo lectus dictum sapien, id scelerisque justo nulla nec justo.</p>
+            </div><!-- /.post-body -->
+
+            <div class="post-meta-2">
+                <ul>
+                    <li><a class="post-tag" href="#">#art</a></li>
+                    <li><a class="post-tag" href="#">#featured</a></li>
+                    <li><a class="post-tag" href="#">#paintbrush</a></li>
+                    <li><a class="post-tag" href="#">#fun</a></li>
+                    <li><a class="post-tag" href="#">#success story</a></li>
+                </ul>
+                <div class="social-block">
+                </div>
+            </div>
+
+            <section class="previous-block">
+                <h4>Similarly awesome...</h4>
+                <ul>
+                    <li>
+                        <h3><a href="#">The Power of Play: Abyssinian &amp; Schoola</a></h3>
+                        <p>12 days ago</p>
+                    </li>
+                    <li>
+                        <h3><a href="#">Beautiful Music: KIPP Academy &amp; Schoola</a></h3>
+                        <p>15 days ago</p>
+                    </li>
+                    <li>
+                        <h3><a href="#">Clothing Drive: Violins and Pastels</a></h3>
+                        <p>16 days ago</p>
+                    </li>
+                </ul>
+                <p><a href="#">See More</a></p>
+            </section>
+
+        </div>
+
+        <div class="blog-sidebar">
+            <div class="bulletin">
+                <p>The latest what nots from Schoola, intro text and stuff. Oh, subscribe via RSS if you swing that way.</p>
+                <h4>Popular Topics</h4>
+                <ul class="popular-topics">
+                    <li><a href="#">#art</a>,</li>
+                    <li><a href="#">#music</a>,</li>
+                    <li><a href="#">#play</a>,</li>
+                    <li><a href="#">#opinio</a>,</li>
+                    <li><a href="#">#sweepstakes</a>,</li>
+                    <li><a href="#">#video</a>,</li>
+                    <li><a href="#">see all</a></li>
+                </ul>
+                <h4>Looking Back</h4>
+                <div class="split-block">
+                    <ul>
+                        <li><a href="#">May 2014</a></li>
+                        <li><a href="#">Jun 2014</a></li>
+                    </ul>
+                    <ul>
+                        <li><a href="#">Jul 2014</a></li>
+                        <li><a href="#">Aug 2014</a></li>
+                    </ul>
+                </div>
+                <h4>Come say Hi!</h4>
+                <div class="split-block">
+                    <ul>
+                        <li><a class="wi" href="#"><i class="fa fa-twitter"></i>@schoola</a></li>
+                        <li><a class="wi" href="#"><i class="fa fa-youtube-play"></i>schoolstories</a></li>
+                    </ul>
+                    <ul>
+                        <li><a class="wi" href="#"><i class="fa fa-facebook-square"></i>/schoola</a></li>
+                        <li><a class="wi" href="#"><i class="fa fa-instagram"></i>@schoola</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="search-block">
+                <form>
+                    <div class="input-w">
+                        <a href="#"><i class="fa fa-search"></i></a>
+                        <input type="text" placeholder="Search Articles..." />
+                    </div>
+                </form>
+            </div>
+        </div>
+
+    </div>
+
+</div><!-- /.container -->
+
+<?php s_footer(); ?>
+</body>
+</html>
